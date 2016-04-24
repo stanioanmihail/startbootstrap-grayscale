b5273657ce25f3ae615ef39f93a738c6cabd26f6
soareionmihai <soare.mihai1992@google.com>
2016-04-24 18:59:14 +0300

    hopefully repaired navbar

--- js/grayscale.js
+++ js/grayscale.js
@@ -4,17 +4,21 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
+(function($) {
+})(jQuery);
+
 // jQuery to collapse the navbar on scroll
 function collapseNavbar() {
     if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $(".navbar-fixed-top").addClass("navbar-main-collapse");
     } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".navbar-fixed-top").removeClass("navbar-main-collapse");
     }
 }
 
-$(window).scroll(collapseNavbar);
-$(document).ready(collapseNavbar);
+
+jQuery(window).scroll(collapseNavbar);
+jQuery(document).ready(collapseNavbar);
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
@@ -26,6 +30,7 @@ $(function() {
         event.preventDefault();
     });
 });
+
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
